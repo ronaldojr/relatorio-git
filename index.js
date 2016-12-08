54d0e788d02a58fa726ac6bbc67bfddee54f553d
RobHawk <rob.hawk@hotmail.com>
2016-12-08 10:35:19 -0200

    setup dev dependencies for dev scope and basic tests for repositories

---
+++ index.js
@@ -0,0 +1,7 @@
+let app = require('./app')
+let dateFormat  = require('dateformat')
+
+app.listen(3000, () => {
+	var date = dateFormat(new Date(), "dd-mm-yyyy HH:MM:ss")
+    console.log('[' + date + '] - ' + 'Servidor relatorio-git online na porta 3000')
+});
